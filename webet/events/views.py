7dd65cea9787b499d80d9178271d2e60e218d912
Will Poorman <will.k.poorman@gmail.com>
2021-04-30 00:31:36 -0500

    feat: Add matches endpoints
    Adds match serializer, view, and register its URLs.
    
    remove unneeded 3rd param from router registers

--- webet/events/views.py
+++ webet/events/views.py
@@ -1,8 +1,21 @@
 from rest_framework import viewsets
 
-from events.serializers import EventSerializer
-from events.models import Event
+from events.serializers import EventSerializer, MatchSerializer
+from events.models import Event, Match
 
 class EventView(viewsets.ModelViewSet):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
+
+class MatchView(viewsets.ModelViewSet):
+    serializer_class = MatchSerializer
+    queryset = Match.objects.all()
+
+    def filter_queryset(self, queryset):
+        """
+        Filter the queryset to only the matches for the given event ID, if one is provided
+        """
+        event_id = self.request.query_params.get('event')
+        if event_id is not None:
+            queryset = queryset.filter(event=event_id)
+        return queryset
--- webet/webet/urls.py
+++ webet/webet/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from events.views import EventView
+from events.views import EventView, MatchView
 from players.views import PlayerView
 
 router = routers.DefaultRouter()
-router.register(r'events', EventView, 'events')
-router.register(r'players', PlayerView, 'players')
+router.register(r'events', EventView)
+router.register(r'matches', MatchView)
+router.register(r'players', PlayerView)
 
 urlpatterns = [
     path('api/', include(router.urls)),
